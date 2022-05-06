e0ed97f74750794af63905802fa1edd13804030c
Kth <vcro12123@naver.com>
2022-05-06 20:33:53 +0900

    fromOnChange 기능 추가

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 const ExpenseForm = () => {
+  // const [enterdTitle, setEnteredTitle] = useState('');
+  // const [enterdAmount, setEnteredAmount] = useState('');
+  // const [enterdDate, setEnteredDate] = useState('');
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enterdAmount: '',
+    enterdDate: '',
+  });
+  const titleChangeHandler = (e) => {
+    setUserInput({ ...userInput, enteredTitle: e.target.value });
+    console.log(userInput);
+  };
+  const amountChangeHandler = (e) => {
+    setUserInput({ ...userInput, enterdAmount: e.target.value });
+  };
+  const dateChangeHandler = (e) => {
+    setUserInput({ ...userInput, enterdDate: e.target.value });
+  };
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type={'text'} />
+          <input type={'text'} onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type={'number'} min={'0.01'} step={'0.01'} />
+          <input
+            type={'number'}
+            min={'0.01'}
+            step={'0.01'}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type={'date'} min={'2019-01-01'} max={'2022-12-31'} />
+          <input
+            type={'date'}
+            min={'2019-01-01'}
+            max={'2022-12-31'}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className='new-expense__actions'>
