2f62d1e07abaacc2166866898c8caee17976dd5b
Kth <vcro12123@naver.com>
2022-05-08 18:05:52 +0900

    상태유지 컴포넌트

--- src/components/Expenses/ExpanseItem.js
+++ src/components/Expenses/ExpanseItem.js
@@ -4,18 +4,11 @@ import './ExpanseItem.css';
 import ExpenseDate from './ExpenseDate';
 
 const ExpanseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    let titles = 'upDeted';
-    setTitle(titles);
-  };
-
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2> {title}</h2>
+        <h2> {props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
--- src/components/Expenses/Expanses.js
+++ src/components/Expenses/Expanses.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpanseItem from './ExpanseItem';
 import Card from '../UI/Card';
 import './Expanses.css';
+import ExpenseFilter from './ExpenseFilter';
 const Expanses = ({ data }) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className='expenses'>
-      {data.map((item) => {
-        return (
-          <ExpanseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          />
-        );
-      })}
-    </Card>
+    <>
+      <Card className='expenses'>
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {data.map((item) => {
+          return (
+            <ExpanseItem
+              key={item.id}
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
+          );
+        })}
+      </Card>
+    </>
   );
 };
 
